f42aa78ccbc13ca0c50c7af4d5d7e6771483fe74
contadofort110275di-dotcom <contadofort110275di@gmail.com>
2026-02-24 11:18:56 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App (){
+  return(
+    <div> 
+      <h1>Aula de React com Vite js</h1>
+      <h2>essa é a linha h2</h2>
+      <p>esse é o paragrafo</p>
+    </div>
+  );
+}
+export default App;
